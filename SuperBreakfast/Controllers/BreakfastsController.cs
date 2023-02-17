7c2a4fe6fa1a4d40dd41a148f852071955077725
terlek <terlek@DESKTOP-RL37VNN>
2023-02-17 08:00:57 +0000

    Updated routing information in controller

--- SuperBreakfast/Controllers/BreakfastsController.cs
+++ SuperBreakfast/Controllers/BreakfastsController.cs
@@ -1,30 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using SuperBreakfast.Contracts.Breakfast;
 
-namespace SuperBreakfast.Controllers;
+namespace SuperBreakfasts.Controllers;
 
 [ApiController]
-public class BreakfastController : ControllerBase
+[Route("[controller]")]
+public class BreakfastsController : ControllerBase
 {
-    [HttpPost("/breakfasts")]
+    [HttpPost()]
     public IActionResult CreateBreakfast(CreateBreakfastRequest request)
     {
         return Ok(request);
     }
 
-    [HttpGet("/breakfasts/{id:guid}")]
+    [HttpGet("{id:guid}")]
     public IActionResult GetBreakfast(Guid id)
     {
         return Ok(id);
     }
 
-    [HttpPut("/breakfasts/{id:guid}")]
+    [HttpPut("{id:guid}")]
     public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequest request)
     {
         return Ok(request);
     }
 
-    [HttpDelete("/breakfasts/{id:guid}")]
+    [HttpDelete("{id:guid}")]
     public IActionResult DeleteBreakfast(Guid id)
     {
         return Ok(id);
