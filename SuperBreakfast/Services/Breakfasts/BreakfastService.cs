9de599a59ababe0039a4701d16c7c9cf8fac5fec
Kterlecki <49212346+Kterlecki@users.noreply.github.com>
2023-02-18 07:53:13 +0000

    Feature/controller (#4)
    * Initial Http methods
    
    * Added Http method for testing purposes
    
    * Updated routing information in controller
    
    * Add model, basic Service and updated createBreakfast
    
    * Add Create and get methods
    
    ---------
    
    Co-authored-by: terlek <terlek@DESKTOP-RL37VNN>

--- SuperBreakfast/Services/Breakfasts/BreakfastService.cs
+++ SuperBreakfast/Services/Breakfasts/BreakfastService.cs
@@ -4,9 +4,18 @@ namespace SuperBreakfast.Services.Breakfasts;
 
 public class BreakfastService : IBreakfastService
 {
-    private readonly Dictionary<Guid, Breakfast> _breakfasts = new();
+
+    private static readonly Dictionary<Guid, Breakfast> _breakfasts = new();
+
     public void CreateBreakfast(Breakfast breakfast)
     {
         _breakfasts.Add(breakfast.Id, breakfast);
     }
+
+    public Breakfast GetBreakfast(Guid id)
+    {
+        return _breakfasts[id];
+        //return _breakfasts.Values.FirstOrDefault(b => b.Id == id) ?? throw new KeyNotFoundException("Breakfast with Id provided, not found");
+    }
+
 }
--- SuperBreakfast/Services/Breakfasts/IBreakfastService.cs
+++ SuperBreakfast/Services/Breakfasts/IBreakfastService.cs
@@ -5,4 +5,6 @@ namespace SuperBreakfast.Services.Breakfasts;
 public interface IBreakfastService
 {
     public void CreateBreakfast(Breakfast breakfast);
+
+    Breakfast GetBreakfast(Guid id);
 }
