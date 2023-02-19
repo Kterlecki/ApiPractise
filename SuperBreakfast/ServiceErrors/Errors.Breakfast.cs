ee04275b7c96b88c253997a5666a4f74f6fdf608
terlek <terlek@DESKTOP-RL37VNN>
2023-02-19 08:46:43 +0000

    Add error hnadling to breakfast class

--- SuperBreakfast/ServiceErrors/Errors.Breakfast.cs
+++ SuperBreakfast/ServiceErrors/Errors.Breakfast.cs
@@ -6,6 +6,16 @@ public static class Errors
 {
     public static class Breakfast
     {
+        public static Error InvalidName => Error.Validation(
+            code: "Breakfast.InvalidName",
+            description: $"Breakfast name must be atleast {Models.Breakfast.MinNameLenght}" +
+            $"and not greater than {Models.Breakfast.MaxNameLenght} characters long"
+        );
+        public static Error InvalidDescription => Error.Validation(
+            code: "Breakfast.InvalidDescription",
+            description: $"Breakfast Description must be atleast {Models.Breakfast.MinDescriptionLenght}" +
+            $"and not greater than {Models.Breakfast.MaxDescriptionLenght} characters long"
+        );
         public static Error NotFound => Error.NotFound(
             code: "Breakfast.NotFound",
             description: "Breakfast not found"
