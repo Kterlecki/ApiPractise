f7be96e4ee25b5a2f16f67b41cc46f65f9846f40
Kterlecki <49212346+Kterlecki@users.noreply.github.com>
2023-02-21 07:02:53 +0000

    Tests (#11)
    * Add negative scenario test
    
    * Add negative scenario test for description property
    
    ---------
    
    Co-authored-by: terlek <terlek@DESKTOP-RL37VNN>

--- test/Modeltests.cs
+++ test/Modeltests.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System;
-using Moq;
 using SuperBreakfast.Models;
 using Xunit;
 using ErrorOr;
@@ -11,7 +10,7 @@ public class ModelTests
 {
     [Fact]
     public void BreakfastCreate_GivenCorrectParamaters_ReturnsBreakfast()
-    {        
+    {
         var name = "Its now dinner time";
         var description = "This breakfast contains nutrients, lots and lost of nutrients";
         var startDateTime = new DateTime(2023, 02, 20, 8, 0, 0);
@@ -30,15 +29,57 @@ public class ModelTests
         var breakfast = result.Value;
 
         Assert.IsType<ErrorOr<Breakfast>>(result);
-        Assert.NotNull(result);
         Assert.Equal(name, breakfast.Name);
         Assert.Equal(description, breakfast.Description);
         Assert.Equal(startDateTime, breakfast.StartDateTime);
         Assert.Equal(endDateTime, breakfast.EndDateTime);
         Assert.Equal(savory, breakfast.Savory);
         Assert.Equal(sweet, breakfast.Sweet);
-        
-       
-        
+    }
+
+    [Fact]
+    public void BreakfastCreate_GivenNameIncorrectParamaters_ReturnsError()
+    {
+        var name = "It";
+        var description = "This breakfast contains nutrients, lots and lost of nutrients";
+        var startDateTime = new DateTime(2023, 02, 20, 8, 0, 0);
+        var endDateTime = new DateTime(2023, 02, 20, 10, 0, 0);
+        var savory = new List<string> { "Oatmeal", "Avocado Toast", "Omelette", "Salad" };
+        var sweet = new List<string> { "Pancakes", "Waffles" };
+
+        var result = Models.Breakfast.Create(name, 
+        description,
+        startDateTime,
+        endDateTime,
+        savory,
+        sweet,
+        Guid.NewGuid()
+        );
+
+        Assert.True(result.IsError);
+
+    }
+
+    [Fact]
+    public void BreakfastCreate_GivenDescriptionIncorrectParamaters_ReturnsError()
+    {
+        var name = "Its now breakfast time";
+        var description = "This breakfast";
+        var startDateTime = new DateTime(2023, 02, 20, 8, 0, 0);
+        var endDateTime = new DateTime(2023, 02, 20, 10, 0, 0);
+        var savory = new List<string> { "Oatmeal", "Avocado Toast", "Omelette", "Salad" };
+        var sweet = new List<string> { "Pancakes", "Waffles" };
+
+        var result = Models.Breakfast.Create(name, 
+        description,
+        startDateTime,
+        endDateTime,
+        savory,
+        sweet,
+        Guid.NewGuid()
+        );
+
+        Assert.True(result.IsError);
+
     }
 }
