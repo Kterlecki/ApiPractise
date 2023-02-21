3a3777392624fc4e61a2a7b236e72cce90f301c7
terlek <terlek@DESKTOP-RL37VNN>
2023-02-21 08:15:52 +0000

    Add DeleteMethod Happy path

--- test/ServiceTests.cs
+++ test/ServiceTests.cs
@@ -36,4 +36,35 @@ public class ServiceTests
     Assert.Equal(0, initialCount);
     Assert.Equal(1, breakfastService.GetDictionaryCount());
     }
+
+
+    [Fact]
+    public void BreakfastDelete_GivenCorrectParamaters_ReturnsBreakfast()
+    {
+        
+       var breakfastService = new BreakfastService();
+       var initialCount = breakfastService.GetDictionaryCount();
+       var breakfast = Breakfast.Create(
+        "Its now dinner time",
+        "This breakfast contains nutrients, lots and lost of nutrients",
+        new DateTime(2023, 02, 20, 8, 0, 0),
+        new DateTime(2023, 02, 20, 10, 0, 0),
+        new List<string> { "Oatmeal", "Avocado Toast", "Omelette", "Salad" },
+        new List<string> { "Pancakes", "Waffles" },
+        Guid.NewGuid()
+    );
+    
+    // Act
+    var createResult = breakfastService.CreateBreakfast(breakfast.Value);
+    var postCreationCount = breakfastService.GetDictionaryCount();
+    var deleteResult = breakfastService.DeleteBreakFast(breakfast.Value.Id);
+    var postDeletionCount = breakfastService.GetDictionaryCount();
+    // Assert
+    Assert.IsType<Deleted>(deleteResult.Value);
+
+    Assert.Equal(0, initialCount);
+    Assert.Equal(1, postCreationCount);
+    Assert.Equal(0, postDeletionCount);
+    }
+
 }
