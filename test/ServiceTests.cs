2ca467abb3b4a153080c9f8e6d29efd1cb2d46ca
terlek <terlek@DESKTOP-RL37VNN>
2023-02-22 07:18:49 +0000

    Negative test for DeleteService

--- test/ServiceTests.cs
+++ test/ServiceTests.cs
@@ -39,7 +39,7 @@ public class ServiceTests
 
 
     [Fact]
-    public void BreakfastDelete_GivenInorrectParamaters_ReturnsNotFound()
+    public void BreakfastDelete_GivenCorrectParamaters_ReturnsNotFound()
     {
        var breakfastService = new BreakfastService();
        var initialCount = breakfastService.GetDictionaryCount();
@@ -64,6 +64,35 @@ public class ServiceTests
     Assert.Equal(0, initialCount);
     Assert.Equal(1, postCreationCount);
     Assert.Equal(0, breakfastService.GetDictionaryCount());
+    }
+
+    [Fact]
+    public void BreakfastDelete_GivenInorrectParamaters_ReturnsValue()
+    {
+       var breakfastService = new BreakfastService();
+       var initialCount = breakfastService.GetDictionaryCount();
+       var breakfast = Breakfast.Create(
+        "Its now dinner time",
+        "This breakfast contains nutrients, lots and lost of nutrients",
+        new DateTime(2023, 02, 20, 8, 0, 0),
+        new DateTime(2023, 02, 20, 10, 0, 0),
+        new List<string> { "Oatmeal", "Avocado Toast", "Omelette", "Salad" },
+        new List<string> { "Pancakes", "Waffles" },
+        Guid.NewGuid()
+    );
+
+    // Act
+    var createResult = breakfastService.CreateBreakfast(breakfast.Value);
+    var postCreationCount = breakfastService.GetDictionaryCount();
+    var deleteResult = breakfastService.DeleteBreakFast(Guid.NewGuid());
+    
+    var postDeletionCount = breakfastService.GetDictionaryCount();
+    // Assert
+    Assert.IsType<Deleted>(deleteResult.Value);
+
+    Assert.Equal(0, initialCount);
+    Assert.Equal(1, postCreationCount);
+    Assert.Equal(1, postDeletionCount);
     }
 
 }
