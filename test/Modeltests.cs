6ccc1fc05b5ff88f2a16ef9a469411950f7258b3
terlek <terlek@DESKTOP-RL37VNN>
2023-02-24 08:23:07 +0000

    Tested From method in model

--- test/Modeltests.cs
+++ test/Modeltests.cs
@@ -3,6 +3,7 @@ using System;
 using SuperBreakfast.Models;
 using Xunit;
 using ErrorOr;
+using SuperBreakfast.Contracts.Breakfast;
 
 namespace SuperBreakfast.tests;
 
@@ -82,4 +83,36 @@ public class ModelTests
         Assert.True(result.IsError);
 
     }
+
+
+    [Fact]
+    public void BreakfastFrom_GivenCorrectValues_ReturnBreakfast()
+    {
+        var name = "Its now dinner time";
+        var description = "This breakfast contains nutrients, lots and lost of nutrients";
+        var startDateTime = DateTime.Now;
+        var endDateTime = DateTime.Now.AddHours(1);
+        var savory = new List<string> { "Bacon", "Sausage" };
+        var sweet = new List<string> { "Maple syrup", "Whipped cream" };
+
+        var request = new CreateBreakfastRequest(
+            Name: name,
+            Description: description,
+            StartDateTime: startDateTime,
+            EndDateTime: endDateTime,
+            Savory: savory,
+            Sweet: sweet
+        );
+
+        var result = Models.Breakfast.From(request);
+
+        Assert.IsType<ErrorOr<Breakfast>>(result);
+        Assert.Equal(name, result.Value.Name);
+        Assert.Equal(description, result.Value.Description);
+        Assert.Equal(startDateTime, result.Value.StartDateTime);
+        Assert.Equal(endDateTime, result.Value.EndDateTime);
+        Assert.Equal(savory, result.Value.Savory);
+        Assert.Equal(sweet, result.Value.Sweet);
+    }
+
 }
